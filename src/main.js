c19725910a41051bd55e8fa43f136409eb95e0a6
tevee <brydiginte69@gmail.com>
2024-02-16 11:23:23 +0100

    ready for deployment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import anime from 'animejs'
 import sunFlowerImgUrl from './media/sunflower.jpg'
 
 const img = document.createElement('img')
 document.body.append(img);
 img.src = sunFlowerImgUrl;
+
+anime({
+    targets: img,
+    rotate: 360
+})
